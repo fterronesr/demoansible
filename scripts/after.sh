0d9d98c966432812c8c788a2945b9fd86a77c385
Marco <mguerrero@caleidos.pe>
2019-04-22 16:58:33 -0500

    test after & before

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
-sleep 12
-pwd > /home/pasatupase/demo/after1.txt
+
+COUNTRIES=("pe/qas")
+ENVIRONMENT="PDP"
+NSSICC_DIRECTORY="/home/oracle/desplieguesnSSiCC/EARs/NSSICC"
+SSICC_DIRECTORY="/home/oracle/desplieguesnSSiCC/EARs/SSICC"
+DEPLOY_DIRECTORY="/home/oracle/desplieguesnSSiCC"
+
+for country in "${COUNTRIES[@]}"; do
+
+	if [ -d "$NSSICC_DIRECTORY" ]; then
+		#WAR directory
+	  	cd "${NSSICC_DIRECTORY}"
+
+	    #rename war
+	    mv nssicc.war "ssicc${country///}.war"
+
+	    #shell path
+	    cd "${DEPLOY_DIRECTORY}"
+
+	    #execute shell
+	    sh ".Auto_nSSICC_${ENVIRONMENT}_AutomaticoQA.sh" 'NSSICC' "${country}"
+
+	    #WAR directory
+	    cd "${NSSICC_DIRECTORY}"
+	    #remove war renamed
+	    mv "ssicc${country///}.war" nssicc.war
+	fi
+
+	if [ -d "$SSICC_DIRECTORY" ]; then
+		#shell path
+		cd "${DEPLOY_DIRECTORY}"
+
+		#execute shell
+		touch "Auto_SSICC_${ENVIRONMENT}_Automatico_${country///}.txt"
+		#sh ".Auto_SSICC_${ENVIRONMENT}_Automatico.sh" 'SSICC' '${country}'
+	fi
+
+done
